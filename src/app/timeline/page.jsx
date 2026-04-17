788e8f97fd2f3c74649b6bf42528c3fd1f815ad5
Mehedi-AgEcon <sourov2454@gmail.com>
2026-04-17 10:44:09 +0200

    Timeline page complete with filtering option

--- src/app/timeline/page.jsx
+++ src/app/timeline/page.jsx
@@ -4,7 +4,8 @@ import { useEffect, useState } from "react";
 import { FcVideoCall } from "react-icons/fc";
 import { HiOutlineChatBubbleBottomCenterText } from "react-icons/hi2";
 import { MdWifiCalling3 } from "react-icons/md";
-import { PiPhoneCallDuotone } from "react-icons/pi";
+import { RiArrowDropDownLine } from "react-icons/ri";
+
 
 const TimelinePage = () => {
   const [timeline, setTimeline] = useState([]);
@@ -15,27 +16,48 @@ const TimelinePage = () => {
 
   }, []);
 
-  return (
-    <div className="max-w-8xl mx-auto px-6 py-10">
-        
-      <h1 className="text-4xl font-bold mb-6 ">Timeline</h1>
+  const [filter, setFilter] = useState("All");
+  const [open, setOpen] = useState(false);
 
-      <div className="bg-base-100 border-gray-400 mb-3 font-semibold text-gray-600 text-xl flex items-end">
-        <h3>Filter</h3>
+  const filteredTimeline = filter === "All" ? timeline : timeline.filter((item) => item.type === filter);
+
+  return (
+    <div className="w-10/12 mx-auto px-6 py-10">
+        
+      <h1 className="text-4xl font-bold mb-6">Timeline</h1>
+
+      <div className="mb-6">
+          <div className="relative w-full max-w-md">
+
+    <select
+      value={filter}
+      onChange={(e) => setFilter(e.target.value)}
+      className="w-full  bg-gray-200 rounded px-4 py-3 text-gray-600 shadow-xl "
+    >
+      <option value="All">Filter timeline</option>
+      <option value="Call">Call</option>
+      <option value="Text">Text</option>
+      <option value="Video">Video</option>
+    </select>
+
+
+    <div className="absolute right-4 top-1/2 -translate-y-1/2 text-gray-400 pointer-events-none">
+      <RiArrowDropDownLine className="p-2"/>
+    </div>
+
+  </div>
       </div>
 
       {timeline.length === 0 ? (
-        <p>No interactions yet</p>
+        <div className="bg-gray-100 shadow p-50 text-center space-y-4">
+          <h2 className="text-4xl font-bold text-gray-600">.....No Interactions Yet...</h2>
+          <button href="/" className="btn btn-dash">Go to Home</button>
+        </div>
       ) : (
-
-
         <div>
-          {timeline.map((item) => (
+          {filteredTimeline.map((item) => (
            
-    <div
-        key={item.id}
-      className="bg-white p-4 gap-150 rounded shadow-sm flex justify-between items-center mt-3"
-    >
+    <div key={item.id} className="bg-white p-4 gap-150 rounded shadow-sm flex justify-between items-center mt-3">
       
       
       <div className="flex gap-4 items-center">
